f966acbfa0141dad2829a2812d9f6953006f89fe
HichamLuffy <imalexvssteve2020@gmail.com>
2024-05-16 20:06:06 +0100

    fix task

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
-"""" encrypt password """
+""" Encrypt password """
 import bcrypt
 
 
-def hash_password(password) -> bytes:
-    """ hash password """
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+def hash_password(password: str) -> bytes:
+    """ Hash a password for storing. """
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validate a password against its hash."""
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
