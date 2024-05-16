30847c40da3e610217b94692aaed2b98f5dad700
HichamLuffy <imalexvssteve2020@gmail.com>
2024-05-16 20:03:44 +0100

    task 5

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""" encrypt password """
+import bcrypt
+
+
+def hash_password(password) -> bytes:
+    """ hash password """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
