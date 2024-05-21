7b61a5a461b06ff0d06313e22a8a9ec3cc15b3a9
HichamLuffy <imalexvssteve2020@gmail.com>
2024-05-21 12:52:27 +0100

    set files

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,12 +3,14 @@
 
 
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
+    """ Return a greeting"""
     return "Hello, World!"
+
 
 if __name__ == '__main__':
     import os
