768c5e33526bce3d68336be0235d60524e6f3c5a
HichamLuffy <imalexvssteve2020@gmail.com>
2024-05-23 22:04:07 +0100

    fix task 8 style

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -33,7 +33,9 @@ def login():
     return response
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'],
+                 strict_slashes=False)
 def logout():
     """Handles user logout"""
     from api.v1.app import auth
