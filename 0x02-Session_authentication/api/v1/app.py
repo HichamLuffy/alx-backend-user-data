7e919b9f266e03c3af1477e5c57337f6047a2bfa
HichamLuffy <imalexvssteve2020@gmail.com>
2024-05-23 18:24:09 +0100

    task 0 Et moi et moi et moi!

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,6 +32,7 @@ def before_request_func():
         return
     if auth.authorization_header(request) is None:
         abort(401)
+    request.current_user = auth.current_user(request)
     if auth.current_user(request) is None:
         abort(403)
 
