ffb9cf67ed3396a38d1d0f00530b30a65e156de1
HichamLuffy <imalexvssteve2020@gmail.com>
2024-05-30 17:58:01 +0100

    login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -9,18 +9,15 @@ AUTH = Auth()
 
 
 @app.route("/sessions", methods=["POST"], strict_slashes=False)
-def login() -> Response:
-    """login"""
+def login():
     email = request.form.get("email")
     password = request.form.get("password")
-
-    if AUTH.valid_login(email, password):
-        jsoni = jsonify({"email": email, "message": "logged in"}), 200
-        response = make_response(jsoni)
-        response.set_cookie("session_id", AUTH.create_session(email))
-        return response
-
-    abort(401)
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 @app.route("/users", methods=["POST"])
